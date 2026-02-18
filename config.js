fdf160d32528fb9aaef3a34e21441d0ca9976b2b
DominicDev <s0l0estudi4r@gmail.com>
2026-02-18 18:18:49 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'PEARL JAM';
+export let nameSongs = [
+	"ALIVE", /*1*/
+	"EVEN FLOW", /*2*/
+	"JEREMY", /*3*/
+	"BLACK", /*4*/
+	"DAUGHTER", /*5*/
+	"BETTER MAN", /*6*/
+	"YELLOW LEDBETTER", /*7*/
+	"LAST KISS", /*8*/
+	"CORDUROY", /*9*/
+	"ANIMAL", /*10*/
+	"DISSIDENT", /*11*/
+	"GARDEN", /*12*/
+	"WHO YOU ARE", /*13*/
+	"WORLD WIDE SUICIDE", /*14*/
+	"NOT FOR YOU", /*15*/
+	"REARVIEWMIRROR", /*16*/
+	"DO THE EVOLUTION", /*17*/
+	"I AM MINE", /*18*/
+	"GO", /*19*/
+	"GIVEN TO FLY", /*20*/
+]
